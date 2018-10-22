d78c3b29ac8a71f83e9f8885614c3e67a80f9408
Zreal <Zenglz_pro@163.com>
2018-10-22 15:43:18 +0800

    prewitt

--- spacesharpen.cpp
+++ spacesharpen.cpp
@@ -46,10 +46,46 @@ void Spacesharpen::on_sharpen_laplace_clicked()
 
 void Spacesharpen::on_sharpen_sobel_clicked()
 {
+    filter_operate filter_x,filter_y;
+    filter_x=sp_func::struct_filter(3,3,"1,0,-1,2,0,-2,1,0,-1");
+    filter_y=sp_func::struct_filter(3,3,"-1,-2,-1,0,0,0,1,2,1");
+    qDebug()<<2;
+    QImage *imgx,*imgy;
+    imgx=sp_func::func_filter_trans(img,filter_x);
+    imgy=sp_func::func_filter_trans(img,filter_y);
+    for(int y=0;y<img->height();y++){
+        for(int x=0;x<img->width();x++){
+            int v_x=qRed(imgx->pixel(x,y));
+            int v_y=qRed(imgy->pixel(x,y));
+            int v=v_x+v_y;
+            new_img->setPixel(x,y,qRgb(v,v,v));
 
+        }
+    }
+    qDebug()<<1;
+    this->fig_spacesharpen->setPixmap(QPixmap::fromImage(*new_img));
+    this->close();
 }
 
 void Spacesharpen::on_sharpen_prewitt_clicked()
 {
+    filter_operate filter_x,filter_y;
+    filter_x=sp_func::struct_filter(3,3,"1,0,-1,1,0,-1,1,0,-1");
+    filter_y=sp_func::struct_filter(3,3,"-1,-1,-1,0,0,0,1,1,1");
+    qDebug()<<2;
+    QImage *imgx,*imgy;
+    imgx=sp_func::func_filter_trans(img,filter_x);
+    imgy=sp_func::func_filter_trans(img,filter_y);
+    for(int y=0;y<img->height();y++){
+        for(int x=0;x<img->width();x++){
+            int v_x=qRed(imgx->pixel(x,y));
+            int v_y=qRed(imgy->pixel(x,y));
+            int v=v_x+v_y;
+            new_img->setPixel(x,y,qRgb(v,v,v));
 
+        }
+    }
+    qDebug()<<1;
+    this->fig_spacesharpen->setPixmap(QPixmap::fromImage(*new_img));
+    this->close();
 }
